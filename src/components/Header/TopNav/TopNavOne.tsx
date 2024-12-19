e7f9902294d0e193ce7967c51d04ceb1ede58924
Mohsandev03 <mohsan.k@itkumail.com>
2024-12-19 12:22:34 +0500

    Top-bar

--- src/components/Header/TopNav/TopNavOne.tsx
+++ src/components/Header/TopNav/TopNavOne.tsx
@@ -21,48 +21,10 @@ const TopNavOne: React.FC<Props> = ({ props, slogan }) => {
             <div className={`top-nav md:h-[44px] h-[30px] ${props}`}>
                 <div className="container mx-auto h-full">
                     <div className="top-nav-main flex justify-between max-md:justify-center h-full">
-                        <div className="left-content flex items-center gap-5 max-md:hidden">
-                            <div
-                                className="choose-type choose-language flex items-center gap-1.5"
-                                onClick={() => {
-                                    setIsOpenLanguage(!isOpenLanguage)
-                                    setIsOpenCurrence(false)
-                                }}
-                            >
-                                <div className="select relative">
-                                    <p className="selected caption2 text-white">{language}</p>
-                                    <ul className={`list-option bg-white ${isOpenLanguage ? 'open' : ''}`}>
-                                        {
-                                            ['English', 'Espana', 'France'].map((item, index) => (
-                                                <li key={index} className="caption2" onClick={() => setLanguage(item)}>{item}</li>
-                                            ))
-                                        }
-                                    </ul>
-                                </div>
-                                <Icon.CaretDown size={12} color='#fff' />
-                            </div>
-                            <div
-                                className="choose-type choose-currency flex items-center gap-1.5"
-                                onClick={() => {
-                                    setIsOpenCurrence(!isOpenCurrence)
-                                    setIsOpenLanguage(false)
-                                }}
-                            >
-                                <div className="select relative">
-                                    <p className="selected caption2 text-white">{currence}</p>
-                                    <ul className={`list-option bg-white ${isOpenCurrence ? 'open' : ''}`}>
-                                        {
-                                            ['USD', 'EUR', 'GBP'].map((item, index) => (
-                                                <li key={index} className="caption2" onClick={() => setCurrence(item)}>{item}</li>
-                                            ))
-                                        }
-                                    </ul>
-                                </div>
-                                <Icon.CaretDown size={12} color='#fff' />
-                            </div>
-                        </div>
+                     
                         <div className="text-center text-button-uppercase text-white flex items-center">
-                            {slogan}
+                        <h3>FREE DELIVERY TO YOU - Get Free Standard Delivery On All Orders See Details</h3>
+
                         </div>
                         <div className="right-content flex items-center gap-5 max-md:hidden">
                             <Link href={'https://www.facebook.com/'} target='_blank'>
