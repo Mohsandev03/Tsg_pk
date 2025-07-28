a755753d6fd97019c106e7eed2b1f0098e9a31e2
Mohsandev03 <mohsan.k@itkumail.com>
2025-07-28 16:04:29 +0500

    3

--- src/app/pages/customer-feedbacks/page.tsx
+++ src/app/pages/customer-feedbacks/page.tsx
@@ -4,9 +4,6 @@ import TopNavOne from "@/components/Header/TopNav/TopNavOne";
 import MenuOne from "@/components/Header/Menu/MenuOne";
 import Breadcrumb from "@/components/Breadcrumb/Breadcrumb";
 import Footer from "@/components/Footer/Footer";
-// import Collection from "@/components/Home1/Collectionreview";
-// import LookBook from "@/components/Home1/LookBook";
-// import Quote from "@/components/Home1/Quote";
 import TestimonialItem from "@/components/Testimonial/TestimonialItem";
 
 import { Swiper, SwiperSlide } from "swiper/react";
@@ -19,18 +16,14 @@ import reviewData from "@/data/Testimonial.json";
 import Collection from "@/components/Home1/Collection";
 import Quote from "@/components/Home1/Quote";
 import Newsletter from "@/components/Home1/Newsletter";
-// import LookBook from "@/components/Home1/LookBook";
 
-interface Props {
-  data: Array<TestimonialType>;
-  limit: number;
-}
-const Testimonial: React.FC<Props> = ({ data = [], limit = 3 }) => {
+const Testimonial = () => {
   const [activeIndex, setActiveIndex] = useState(0);
 
   const handleSlideChange = (swiper: any) => {
     setActiveIndex(swiper.activeIndex);
   };
+
   return (
     <>
       <TopNavOne
@@ -47,15 +40,15 @@ const Testimonial: React.FC<Props> = ({ data = [], limit = 3 }) => {
       </div>
       <Quote />
 
-      <div className="customer-feedbacks pt-10  ">
+      <div className="customer-feedbacks pt-10">
         <div className="container">
           <Collection />
         </div>
       </div>
 
-      <div className="customer-feedbacks pb-10   ">
-        <div className="heading3 text-center   ">What People Are Saying</div>
-        <div className="container pagination-mt40 md:mt-10   ">
+      <div className="customer-feedbacks pb-10">
+        <div className="heading3 text-center">What People Are Saying</div>
+        <div className="container pagination-mt40 md:mt-10">
           <Swiper
             slidesPerView={3}
             spaceBetween={30}
@@ -65,15 +58,15 @@ const Testimonial: React.FC<Props> = ({ data = [], limit = 3 }) => {
             className="list-review"
             breakpoints={{
               0: {
-                slidesPerView: 1, // For screens smaller than 640px
+                slidesPerView: 1,
                 spaceBetween: 10,
               },
               640: {
-                slidesPerView: 2, // For screens 640px and larger
+                slidesPerView: 2,
                 spaceBetween: 20,
               },
               1024: {
-                slidesPerView: 3, // For screens 1024px and larger
+                slidesPerView: 3,
                 spaceBetween: 30,
               },
             }}
@@ -86,13 +79,10 @@ const Testimonial: React.FC<Props> = ({ data = [], limit = 3 }) => {
           </Swiper>
         </div>
       </div>
-      <div className="py-10">
-        {/* <LookBook data={[]} start={0} limit={0} /> */}
-      </div>
-      <Newsletter props="bg-green   mb-10" />
+
+      <Newsletter props="bg-green mb-10" />
       <Instagram />
       <Brand />
-
       <Footer />
     </>
   );
